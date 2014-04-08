8f1d9808acf6977f7e48a782d331295097427ce9
klghlch <kaleighleach@gmail.com>
2014-04-08 19:24:01 -0400

    RSVP form

---
+++ contact.php
@@ -0,0 +1,9 @@
+<div class="span8" id="divMain">
+
+    <h1>RSVP</h1>
+    <h3><?php echo $_GET[msg];?></h3>
+    <hr>
+    <h3><a href="index.html">back</a></h3>
+
+                         
+</div>
--- email/index.php
+++ email/index.php
@@ -4,10 +4,11 @@
     $name = $_POST['name'];
 	$email = $_POST['email'];
 	$query = $_POST['message'];
+	$song = $_POST['song'];
 	$email_from = $name.'<'.$email.'>';
 
  $to="kaleighleach@gmail.com";
- $subject="Enquiry!";
+ $subject="Wedding RSVP";
  $headers  = 'MIME-Version: 1.0' . "\r\n";
  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
  $headers .= "From: ".$email_from."\r\n";
@@ -16,17 +17,20 @@
  		 Name:
 		 $name 	   
          <br>
- 		 Email-Id:
+ 		 Email:
 		 $email 	   
          <br>
- 		 Message:
-		 $query 	   
+ 		 Guests Attending:
+		 $query 
+		 <br>
+		 Song Request:
+		 $song	   
       
    ";
 	if(mail($to,$subject,$message,$headers))
-		header("Location:../contact.php?msg=Successful Submission! Thankyou for contacting us.");
+		header("Location:../contact.php?msg=Thank you so much for your RSVP!");
 	else
-		header("Location:../contact.php?msg=Error To send Email !");
+		header("Location:../contact.php?msg=Error To send Email, please try again, or email rsvp@billashleywedding.com");
 		//contact:-your-email@your-domain.com
  }
 ?>
