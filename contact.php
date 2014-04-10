76ec72246d485055f1710da0da06b9f9441434b4
klghlch <kaleighleach@gmail.com>
2014-04-10 10:07:35 -0400

    edits

--- contact.php
+++ contact.php
@@ -1,9 +1,18 @@
-<div class="span8" id="divMain">
+<body  style="background: url(../img/bg.jpg) no-repeat center center fixed; ">
+    
 
-    <h1>RSVP</h1>
-    <h3><?php echo $_GET[msg];?></h3>
-    <hr>
-    <h3><a href="index.html">back</a></h3>
 
-                         
-</div>
+    <div class="span8 text-center" id="divMain" style=" text-transform: uppercase;
+  letter-spacing: 1px;
+  font-size: 1.25em;
+  font-weight: 400;">
+
+        <h1>RSVP</h1>
+        <h3><?php echo $_GET[msg];?></h3>
+        <hr>
+        <h3><a href="index.html">back</a></h3>
+
+                             
+    </div>
+
+</body>
